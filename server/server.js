7a7fcedf24cc58874530f0774e34841f4bb2c755
Jackson Kalmbach <jacksonrkalmbach@gmail.com>
2022-11-22 19:48:57 -0500

    begin building up backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
 app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
 
 
 app.listen(port, () => {
