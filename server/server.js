1b8cf76671fb6cb137db7fc587f57600d7f1baaf
Jackson Kalmbach <jacksonrkalmbach@gmail.com>
2022-11-22 20:19:56 -0500

    baby got backend

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ const port = 3000;
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res
+    .status(200)
+    .send('What\'s shakin, bacon? 🥓');
 })
-
-
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}.`)
